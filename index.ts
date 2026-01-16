cea1a47edfcb3571881e2000040f9fa1dae33699
Jakub Pruszyski <jakub.pruszynski@pragmaticcoders.com>
2026-01-16 22:19:20 +0700

    Impro

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello World From GHCR!"));
+app.get("/", (c) => {
+  const secret = process.env.SAMPLE_SECRET ?? "not set";
+  return c.text(`Hello World From GHCR! SAMPLE_SECRET: ${secret}`);
+});
 
 export default app;
